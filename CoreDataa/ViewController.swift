87a4b5b5491ea7f7b3ba35c8f0e2b06b2625c2c4
Shyngys <ktl.shyngys@gmail.com>
2020-07-31 02:30:04 +0500

    Initial Commit

---
+++ CoreDataa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataa
+//
+//  Created by Shyngys Saktagan on 7/31/20.
+//  Copyright © 2020 ShyngysSaktagan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
